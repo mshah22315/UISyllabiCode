bce4cdf8710ad704e313c6b0c0333cac6dba7f9e
Manthan Shah <manthanshah22@outlook.com>
2025-04-09 19:04:09 -0500

    pulled list of course codes

--- test.py
+++ test.py
@@ -10,14 +10,19 @@ supabase: Client = create_client(url, key)
 offset = 0
 limit = 1000
 all_data = []
+list_of_course_codes = []
 
 while True:
     response = supabase.table("courses_new").select("course_code").range(offset, offset + limit - 1).execute()
     data = response.data
+    for row in data:
+        course_code = row["course_code"]
+        if course_code not in list_of_course_codes:
+            list_of_course_codes.append(course_code)
     if not data:  # Break when no more rows are returned
         break
     all_data.extend(data)
     offset += limit
 
-print(all_data)
+print(list_of_course_codes)
 print(f"Total rows fetched: {len(all_data)}")
