94dea72ef3431db85ac6579720b18d03a1ca40bf
ybandla <ybandla@uiowa.edu>
2025-04-16 18:02:06 -0500

    ts

--- uisyllabi/app/uisyllabi/page.tsx
+++ uisyllabi/app/uisyllabi/page.tsx
@@ -1,8 +1,3 @@
-import { createClient } from '@/utils/supabase/server'
-
 export default async function Page() {
-  const supabase = await createClient()
-  const { data: courses } = await supabase.from('coursesv2').select()
-
-  return <pre>{JSON.stringify(courses, null, 2)}</pre>
+  return <div>hello</div>
 }
